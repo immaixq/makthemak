5c74c7281d1371705432390ca95de534a77409ff
Mai Xueqiao <maixueqiao@Mais-MacBook-Air.local>
2023-11-13 13:09:33 +0800

    Add Footer

---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+// components/Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => (
+  <footer style={{ textAlign: 'center', padding: '50px'}}>
+    <p>
+      Made with 🚀 and a sprinkle of ✨ by Mai. Happy coding!
+    </p>
+  </footer>
+);
+
+export default Footer;
