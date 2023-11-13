636a39eeb72c0d1182fd0d897dda59a7dc2e683a
Mai Xueqiao <maixueqiao@Mais-MacBook-Air.local>
2023-11-13 22:17:11 +0800

    Update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,9 +9,10 @@ export default function about() {
             <div> 
                 <h1 className="font-bold text-5xl font-serif">About Me</h1>
                 <p className="my-6">
-                    Hello, nice to meet you! I am a data scientist based in Singapore. 
-                    This space helps me to document and share
-                    some of the things I learned or thoughts I have. 
+                    Hey there, fellow data explorers! 🚀 I'm Mai, your friendly neighborhood data scientist  based in the vibrant city of Singapore. 
+                    Think of this blog as my magical diary where I spill the beans on all the nerdy spells I've mastered and the whimsical thoughts that pop into my head.
+                    Whether it's deciphering data mysteries or having an existential crisis over a missing semicolon, this space is where I pour out my heart and code.
+                    Expect a delightful blend of tech wisdom, quirky anecdotes, and the occasional data-driven dad joke.
                 </p>
                 <hr />
                 <p className="my-6">
@@ -19,11 +20,11 @@ export default function about() {
                     they can combine to make our world a better place.
                     <span className="dark:text-amber-400 text-blue-700"> It would make my day if you find anything interesting here. </span>
                 </p>
-                <p className="my-6">
+                {/* <p className="my-6">
                     I also find great joy in the process of making things. Whether it is developing a new feature for an application or building this website from scratch, <span className="dark:text-amber-400 text-blue-700">there is something incredibly satisfying about seeing an idea come to life through code.</span>
                     I believe that the ability to create something from nothing is what sets coding apart from other fields, and the prospect of creating something that touches somebody&apos;s life
                     motivates me to continue learning and improving my skills.  I hope to share with others as I continue to grow and develop in this exciting field.
-                </p>
+                </p> */}
             </div>
         </section>
     )
