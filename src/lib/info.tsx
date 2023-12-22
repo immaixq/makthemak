b894f3373f43a46179dd9ca0d51347d50e5168c1
Mai Xueqiao <maixueqiao@Mais-MacBook-Air.local>
2023-12-22 15:18:16 +0800

    Modify info info content

--- src/lib/info.tsx
+++ src/lib/info.tsx
@@ -4,7 +4,7 @@ import me from '../app/avatar.png';
 export const about = () => {
   return(
   <>
-    I&apos;m Mai, a data scientist based in Singapore. This is a space where I share my thoughts, experiences, and musings on a variety of topics that are near and dear to my heart. 
+    A data scientist based in Singapore. This is a space where I share my thoughts, experiences, and musings on a variety of topics that are near and dear to my heart. 
   </>
   )
 }
