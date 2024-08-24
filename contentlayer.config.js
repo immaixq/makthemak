8b46954fcfb16f11c3d9c8c7df538c16b6490734
immaixq <immaixq@me.com>
2024-08-25 01:09:03 +0800

    chore: Update contentlayer.config.js for new content types

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -5,82 +5,82 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 const computedFields = {
-    slug: {
-        type: 'string',
-        // text.mdx => text
-        resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
-    }
+  slug: {
+    type: 'string',
+    // text.mdx => text
+    resolve: (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, ''),
+  }
 }
 
 export const Blog = defineDocumentType(() => ({
-    name: 'Blog',
-    //location of file wrt contentdirpath
-    filePathPattern: 'blog/*.mdx',
-    contentType: 'mdx',
-    fields: {
-        title: {
-            type: 'string',
-            required: true
-        },
-        publishedAt: {
-            type: 'string',
-            required: true
-        },
-        revisedAt: {
-            type: 'string'
-        },
-        summary: {
-            type: 'string',
-            required: true
-        },
-        tags: {
-            type: 'list',
-            of: {type: 'string'}
-        },
-        likes: {
-            type: 'number',
-            default: 0
-        }
+  name: 'Blog',
+  //location of file wrt contentdirpath
+  filePathPattern: 'blog/*.mdx',
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true
     },
-    computedFields
+    publishedAt: {
+      type: 'string',
+      required: true
+    },
+    revisedAt: {
+      type: 'string'
+    },
+    summary: {
+      type: 'string',
+      required: true
+    },
+    tags: {
+      type: 'list',
+      of: { type: 'string' }
+    },
+    likes: {
+      type: 'number',
+      default: 0
+    }
+  },
+  computedFields
 }));
 
 export default makeSource({
-    contentDirPath: 'src/content',
-    documentTypes: [Blog],
-    mdx: {
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [
-        rehypeSlug,
-        [
-          rehypePrettyCode,
-          {
-            theme: 'one-dark-pro',
-            onVisitLine(node) {
-              // Prevent lines from collapsing in `display: grid` mode, and allow empty
-              // lines to be copy/pasted
-              if (node.children.length === 0) {
-                node.children = [{ type: 'text', value: ' ' }];
-              }
-            },
-            onVisitHighlightedLine(node) {
-              node.properties.className.push('line--highlighted');
-            },
-            onVisitHighlightedWord(node) {
-              node.properties.className = ['word--highlighted'];
-            },
+  contentDirPath: 'src/content',
+  documentTypes: [Blog],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypePrettyCode,
+        {
+          theme: 'one-dark-pro',
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: 'text', value: ' ' }];
+            }
           },
-        ],
-        [
-          rehypeAutolinkHeadings,
-          {
-            properties: {
-              className: ['anchor'],
-            },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push('line--highlighted');
           },
-        ],
-    
+          onVisitHighlightedWord(node) {
+            node.properties.className = ['word--highlighted'];
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ['anchor'],
+          },
+        },
+      ],
+
     ]
-    },
-  });
+  },
+});
 
